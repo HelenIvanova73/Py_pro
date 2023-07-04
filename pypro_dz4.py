93add8928ef891f76b1c7ababad3bf604b212e6e
Helen Ivanova <helen.zakhmatova@mail.ru>
2023-07-04 11:54:19 +0400

    создаем READMY дом задание по 5 семинару

--- pypro_dz4.py
+++ pypro_dz4.py
@@ -1,7 +1,7 @@
 '''1. Напишите функцию для транспонирования матрицы'''
 
 
-NUMBER_OF_ROWS = 5
+NUMBER_OF_ROWS = 3
 NUMBER_OF_COLUMNS = 5
 MIN_NUMBER = -10
 MAX_NUMBER = 10
@@ -10,12 +10,12 @@ from random import randint
 
 
 def create_matrix() -> list[list[int]]:
-    return [[randint(MIN_NUMBER, MAX_NUMBER + 1) for _ in range(NUMBER_OF_COLUMNS)] for _ in range(NUMBER_OF_ROWS)]
+     my_list = [[randint(MIN_NUMBER, MAX_NUMBER + 1) for _ in range(NUMBER_OF_COLUMNS)] for _ in range(NUMBER_OF_ROWS)]
+     return my_list
 
 
 def print_matrix(my_list: list[list[int]]):
     nor = len(my_list)
-
     noc = len(my_list[0])
     for i in range(nor):
         for j in range(noc):
@@ -23,24 +23,41 @@ def print_matrix(my_list: list[list[int]]):
         print()
 
 
-def matrix_transposition(my_list):# list[list[int]]) -> list[list[int]]:
-    a = [[0 * 5]] * 5
-    for i in range(4):#NUMBER_OF_ROWS):
-        for j in range(4):#NUMBER_OF_COLUMNS):
-            a[j][i] = 5#my_list[i][j]
-    return type(a)
+def matrix_transposition(my_list: list[list[int]]) -> list[list[int]]:
+    a = [[0 for i in range(NUMBER_OF_ROWS)] for j in range(NUMBER_OF_COLUMNS)]
+    for i in range(NUMBER_OF_ROWS):
+        for j in range(NUMBER_OF_COLUMNS):
+            a[j][i] = my_list[i][j]
+    return a
 
-print_matrix(create_matrix())
+
+b = create_matrix()
+print_matrix(b)
 print()
-print(matrix_transposition(create_matrix()))
-
-
+print(print_matrix(matrix_transposition(b)))
 
 
 '''2.Напишите функцию принимающую на вход только ключевые
 параметры и возвращающую словарь, где ключ — значение
 переданного аргумента, а значение — имя аргумента. Если
 ключ не хешируем, используйте его строковое представление.'''
+
+#salarys[i] * (decimal.Decimal(prem[i]) / 100 + 1)
+def dict_transpositions(**kwards) -> dict:
+    my_dict = {value: (str(key) if key.__hash__() == None else key)for key, value in kwards.items()}
+    print(my_dict)
+    return my_dict
+
+print(dict_transpositions(a=5, b='Hello world!', c=[1, 2, 3], d=('Tom', 'Pit'), e=3.6, f={1, 1, 2, 5}, g=True))
+
+
+
+
+
+
+
+
+
 
 '''3. Напишите программу банкомат. 
 ✔ Начальная сумма равна нулю
